9d7d5be63e1459cd2c15b7f91710a2d40695c9f2
WooHey-P <dkqnwlnd@naver.com>
2024-02-19 12:17:53 +0900

    Compose State
    - compose 에서의 상태값 컨트롤

--- app/src/main/java/com/example/jetapckcomposeexample01/MainActivity.kt
+++ app/src/main/java/com/example/jetapckcomposeexample01/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,23 +56,31 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row {
+        Row(
+            modifier
+                .padding(24.dp)
+        ) {
             Column(
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(24.dp)
+                    .padding(bottom = extraPadding)
                     .weight(1f)
             ) {
                 Text(text = "Hello")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Click me")
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(text = "Show ${if (expanded.value) "Less" else "More"}")
             }
         }
     }
